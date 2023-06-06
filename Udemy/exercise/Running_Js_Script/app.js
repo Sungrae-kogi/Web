3eb7c6bf769aaad4656bb049e9d099cf18e82e0b
Josh_corgi <whvkek@naver.com>
2023-06-07 00:32:22 +0900

    JS

---
+++ Udemy/exercise/Running_Js_Script/app.js
@@ -0,0 +1,31 @@
+console.log("Before Conditional");
+
+let randInt = Math.random();
+if(randInt < 0.5) {
+    console.log("number is less than 0.5");
+}else {
+    console.log("number is more than 0.5");
+}
+
+console.log("After conditional");
+
+// 요일 판독
+const dayOfWeek = prompt("Enter A Day").toLowerCase();
+
+if (dayOfWeek === 'monday'){
+    console.log('I Hate Monday.');
+}else if (dayOfWeek === 'saturday') {
+    console.log('I Love Saturdays.');
+}else {
+    console.log('meh');
+}
+
+
+//패스워드 공백 판독
+const password = prompt("Enter your password");
+
+if(password.length >= 6 && password.indexOf(' ') === -1){
+    console.log("VALID PASSWORD");
+}else {
+    console.log("Incorrect Format For Password");
+}
