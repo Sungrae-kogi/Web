864ba9df0b2c78a19aa634f74c21c9dea3412b9e
Josh_corgi <whvkek@naver.com>
2023-09-02 00:49:00 +0900

    EJS template

---
+++ Udemy/exercise/npm/Ejs/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+//EJS 사용 알림
+//디렉토리 설정
+//https://velog.io/@untiring_dev/Express-EJS-views-%EB%94%94%EB%A0%89%ED%86%A0%EB%A6%AC-%EC%84%A4%EC%A0%95
+app.set('view engine', 'ejs');
+//현재 실행중인 파일(index.js)이 있는 현재 디렉토리(__dirname)와 home.ejs에 도달하기 위한 나머지 경로인 /views를 붙여줌
+app.set('views', path.join(__dirname, '/views'));
+
+
+//simple route
+app.get('/', (req, res) => {
+    const pageNum = "two".toUpperCase();
+    res.render("home", { pageNum: pageNum });
+})
+
+app.get('/rand', (req, res) => {
+    const num = Math.floor(Math.random() * 10) + 1;
+    //템플릿 렌더링에 전달할때 key-value 쌍 객체로 전달
+    res.render('random', { rand: num });
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000");
+})
