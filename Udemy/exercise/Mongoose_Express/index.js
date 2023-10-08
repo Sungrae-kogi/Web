d6c54e37592d29ce6d25485e3f8c605fea3f58a2
Josh_corgi <whvkek@naver.com>
2023-10-09 02:07:53 +0900

    Express, mongoDB 연동 예제

--- Udemy/exercise/Mongoose_Express/index.js
+++ Udemy/exercise/Mongoose_Express/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const methodOverride = require('method-override');
 const Product = require('./models/product');
 
 //mongo
@@ -22,17 +23,58 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand', { useNewUrlParser: true,
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+const categories = ['fruit', 'vegetables', 'dairy'];
 
 //라우트를 위한 비동기 콜백 패턴
 app.get('/products', async (req, res) => {
-    const products = await Product.find();
-    res.render('products/index', { products });
+    const {category} = req.query
+    if(category){
+        const products = await Product.find({category});
+        res.render('products/index', { products, category });
+    }else {
+        const products = await Product.find({});    //모든 상품
+        res.render('products/index', { products, category: 'All'});
+    }
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new', { categories });
+})
+
+//new.ejs에서 입력한 정보를 제출할 라우트
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
 })
 
 app.get('/products/:id', async (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/show', {product});
+    res.render('products/show', { product });
+})
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product, categories });
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+
+    res.redirect(`/products/${product._id}`);
+})
+
+//DELETE요청에 시간이 걸리므로 비동기함수
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
 })
 
 app.listen(3000, () => {
