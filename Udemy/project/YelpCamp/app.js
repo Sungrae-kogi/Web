510b1225d82d0a9d4ea3e15b406910d6f65d5176
Josh_corgi <whvkek@naver.com>
2023-10-19 02:01:55 +0900

    YelpCamp Restful CRUD

--- Udemy/project/YelpCamp/app.js
+++ Udemy/project/YelpCamp/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
@@ -27,6 +28,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
 //res.render("ejs파일경로", {데이터이름표: 전송할 데이터})
 
 app.get('/', (req, res) => {
@@ -43,10 +47,35 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
 
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
 });
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
+
+//npm i method-override
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const {id} = req.params;
+    await Campground.findByIdAndDelete(id);
+    res.redirect('/campgrounds');
+})
 
 
 app.listen(3000, () => {
