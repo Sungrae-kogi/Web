db524188655e3c52d4f12ae6f59cf0a5207f6cab
Josh_corgi <whvkek@naver.com>
2024-02-16 23:33:24 +0900

    validation Joi library

--- Udemy/project/YelpCamp/app.js
+++ Udemy/project/YelpCamp/app.js
@@ -3,6 +3,10 @@ const path = require('path');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const Joi = require('joi');
+const { campgroundSchema } = require('./schemas.js');
+const ExpressError = require('./utils/ExpressError');
+const catchAsync = require('./utils/catchAsync');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://127.0.0.1:27017/YelpCamp', { useNewUrlParser: true, useUnifiedTopology: true });
@@ -33,6 +37,19 @@ app.set('views', path.join(__dirname, '/views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const validateCampground = (req, res, next) => {
+
+    //result 는 콘솔출력시켜보면 error,value field로 되어있다.
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        //비동기 함수 내에 있으니 오류가 발생한다면 에러처리 handler로 갈것.
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
 //res.render("ejs파일경로", {데이터이름표: 전송할 데이터})
 
 app.get('/', (req, res) => {
@@ -50,34 +67,49 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
+
+
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-})
 
-app.get('/campgrounds/:id', async (req, res) => {
+}));
+
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/edit', { campground });
-})
+}));
 
 //npm i method-override
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${campground._id}`);
-})
+}));
 
-app.delete('/campgrounds/:id', async (req, res) => {
-    const {id} = req.params;
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
-})
+}));
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+
+});
+
+app.use((err, req, res, next) => {
+    //비동기함수 내에서 발생한 에러를 전달받는다.
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Oh No, Something went wrong';
+    res.status(statusCode).render('error', { err });
+});
 
 app.listen(3000, () => {
     console.log('Serving on Port 3000');
