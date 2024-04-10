c23cb46fbae15506397054c1d5c6b5437a710ce0
Josh_corgi <whvkek@naver.com>
2024-04-11 00:52:33 +0900

    Auth login,out by session.user_id

--- Udemy/exercise/AuthDemo/index.js
+++ Udemy/exercise/AuthDemo/index.js
@@ -3,6 +3,8 @@ const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
+
 
 // MongoDB 연결 주소
 mongoose.connect('mongodb://127.0.0.1:27017/loginDemo', {})
@@ -24,7 +26,7 @@ app.set('views', 'views');
 
 //urlencoded 미들웨어는 POST 요청의 본문을 파싱하여 req.body객체에 저장합니다, 이후 POST요청 핸들러에서 req.body를 통해 url-encoded데이터에 접근할 수 있습니다.
 app.use(express.urlencoded({ extended: true }));
-
+app.use(session({secret: 'notagoodsecret'}));
 
 app.get('/', (req, res) => {
     res.send("THIS IS THE HOME PAGE");
@@ -35,6 +37,7 @@ app.get('/register', (req, res) => {
 })
 
 //사용자 등록과정이 쿼리문에 들어가면안되니 post 그리고 사용자 등록과 저장과정이필요하니 async 함수
+// 유사 회원가입
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
     const salt = await bcrypt.genSalt(10);
@@ -45,6 +48,7 @@ app.post('/register', async (req, res) => {
             password: hash
         })
         await user.save();
+        req.session.user_id = user._id;  //회원가입을 했으면 그 세션을 유지하도록
         res.redirect('/');
     } catch (error) {
         console.error('해시 생성 중 오류 발생:', error);
@@ -53,9 +57,39 @@ app.post('/register', async (req, res) => {
 
 })
 
+app.get('/login', (req,res) => {
+    res.render('login');
+})
+
+// 로그인 
+app.post('/login', async (req,res) => {
+    const {username, password} = req.body;
+    const user = await User.findOne({username: username});
+    const validPassword = await bcrypt.compare(password, user.password);
+    if(validPassword){
+        //로그인-> 세션에 사용자 id 지정  (세션을 유지하도록)
+        req.session.user_id = user._id;
+        res.redirect('/secret');
+    }else{
+        res.redirect('/login');
+    }
+})
+
+//로그아웃은 주로 post라우트 get만 아니면된다.
+app.post('/logout', (req,res) => {
+    //로그아웃 절차는 세션에서 user_id를 제거하는거가 끝 반드시 null로 설정해야한다.
+    req.session.user_id = null;
+
+    //req.session.destroy(); 라는 방법도있다, 세션 전체를 파기, 하지만 인증을 위한 최소값은 사용자 id추적을 그만두는것이다.
+    
+    res.redirect('/login');
+})
 
 app.get('/secret', (req, res) => {
-    res.send("THIS IS SECRET! YOU CANNOT SEE UNLESS YOU ARE PASSED");
+    if(!req.session.user_id){
+        return res.redirect('/login');
+    }
+    res.render('secret');
 })
 
 app.listen(3000, () => {
