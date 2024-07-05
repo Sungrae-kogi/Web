845ab622fd3c0cd37f5ec4aa4a2cfa85d222ee95
Josh_corgi <whvkek@naver.com>
2024-07-05 17:32:07 +0900

    0705_codeKata

---
+++ SpartaCoding/CodeKata/40_codeKata.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        List<Integer> num = new ArrayList<>();
+        
+        
+        while(n>=3){
+            num.add(n%3);
+            n = n/3;
+        }
+        num.add(n);
+        
+        int sum =0;
+        for(int i=0; i<num.size(); i++){
+            int res = (int) Math.pow(3,num.size()-i-1);
+            sum = sum + res*num.get(i);
+        }
+        
+        return sum;
+    }
+}
