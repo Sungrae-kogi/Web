f43b59bc563fb4ad27bc799dd13e3bf1a429e1d0
Josh_corgi <whvkek@naver.com>
2024-07-08 16:02:59 +0900

    38_codeKata

---
+++ SpartaCoding/CodeKata/SQL/38_codeKata.sql
@@ -0,0 +1,10 @@
+//조건별로 분류하여 주문상태 출력하기
+
+//CASE 문을 사용한 조건분류, ISNULL() 사용한 널 처리.
+SELECT ORDER_ID, PRODUCT_ID, DATE_FORMAT(OUT_DATE, '%Y-%m-%d') AS OUT_DATE, (CASE
+ WHEN OUT_DATE <= '2022-05-01' THEN '출고완료'
+ WHEN ISNULL(OUT_DATE) = 1 THEN '출고미정'
+ ELSE '출고대기'
+ END) AS 출고여부
+FROM FOOD_ORDER
+ORDER BY ORDER_ID;
