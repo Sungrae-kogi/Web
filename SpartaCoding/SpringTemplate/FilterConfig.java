c8f3345780dbf32dad11307afb78e52de234e42f
Josh_corgi <whvkek@naver.com>
2024-09-16 23:23:40 +0900

    spring template

---
+++ SpartaCoding/SpringTemplate/FilterConfig.java
@@ -0,0 +1,16 @@
+@Configuration
+@RequiredArgsConstructor
+public class FilterConfig {
+
+    private final JwtUtil jwtUtil;
+
+    // Filter 등록
+    @Bean
+    public FilterRegistrationBean<JwtFilter> jwtFilter() {
+        FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new JwtFilter(jwtUtil));
+        registrationBean.addUrlPatterns("/*");
+
+        return registrationBean;
+    }
+}
